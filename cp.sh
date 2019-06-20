2879f123aa2a160a983d92970c0bff0c0c2b283c
Zhi Sun <inhzus@gmail.com>
2019-06-20 19:21:31 +0800

    [Conf] startup exec

--- cp.sh
+++ cp.sh
@@ -2,5 +2,5 @@
 cp ~/.config/fusuma/config.yml .config/fusuma/
 cp ~/.ssh/config .ssh/
 cp ~/.gitconfig ~/.vimrc ~/.zshrc .
-cp ~/scripts/android.sh ~/scripts/clash.py ./scripts/
+cp ~/scripts/notify.sh ~/scripts/volume.sh ~/scripts/android.sh ~/scripts/clash.py ./scripts/
 cp ~/.i3/config .i3/
