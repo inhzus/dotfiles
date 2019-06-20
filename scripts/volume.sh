ec1f64318e6b5403a0b6bb3e019489a72795a987
Zhi Sun <inhzus@gmail.com>
2019-06-20 19:20:41 +0800

    [Conf] volume scripts && remove volumeicon

---
+++ scripts/volume.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Pulse Audio Controls
+#"notify-send "Volume Level: `amixer sset Master,0 5- unmute | awk -F'[][]' '/dB/ { print $2 }'`""
+
+case $1 in
+    raise)
+    sh $HOME/scripts/notify.sh -R $HOME/.cache/volume.notify "Volume Level: `amixer sset Master,0 5+ unmute | awk -F'[][]' '/dB/ { print $2 }'`" 2>/dev/null
+    ;;
+    lower)
+    sh $HOME/scripts/notify.sh -R $HOME/.cache/volume.notify "Volume Level: `amixer sset Master,0 5- unmute | awk -F'[][]' '/dB/ { print $2 }'`" 2>/dev/null
+    ;;
+    toggle)
+    sh $HOME/scripts/notify.sh -R $HOME/.cache/volume.notify "Volume: `amixer sset Master,0 toggle | awk '/dB/ { print $6 }'`" 2>/dev/null
+esac
+
+
