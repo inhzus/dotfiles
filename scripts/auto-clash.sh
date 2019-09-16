1369f097f462a04a8b6aa7e08d2e091cfd667b3b
Zhi Sun <inhzus@gmail.com>
2019-09-16 15:46:43 +0800

    [Conf] auto start clash when network connected & default browser

---
+++ scripts/auto-clash.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+while true; do
+  if ping -q -c 1 -W 1 ip.gs >/dev/null; then
+    echo "The network is up"
+    pm2 -a restart ${HOME}/go/bin/clash
+    break
+  fi
+  sleep 1
+done
+
