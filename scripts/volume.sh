21e1f965379b9540c5f836047de864d9b7ae3c98
Zhi Sun <inhzus@gmail.com>
2019-09-20 17:27:22 +0800

    [Conf] i3x-status support for volume

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -2,6 +2,7 @@
 # Pulse Audio Controls
 #"notify-send "Volume Level: `amixer sset Master,0 5- unmute | awk -F'[][]' '/dB/ { print $2 }'`""
 
-
-sh $HOME/scripts/notify.sh -R $HOME/.cache/notify/volume "Volume Level: `amixer sset Master,0 $1 unmute | awk -F'[][]' '/dB/ { print $2 }'`" 2>/dev/null
+vol=`amixer sset Master,0 $1 unmute | awk -F'[][]' '/\[on\]/ {print $2; exit}'`
+echo "volume|vol:$vol" > /tmp/i3x.status
+sh $HOME/scripts/notify.sh -R $HOME/.cache/notify/volume "Volume Level: $vol" 2>/dev/null
 
