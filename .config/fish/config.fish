ea2c46d00fdda86cad9b6a93558611d55c949f20
inhzus <inhzus@outlook.com>
2025-02-05 15:40:40 +0800

    fish shell config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,14 @@
 
+set -x EDITOR vim
+set -x LC_ALL en_US.UTF-8
+set fish_command_timer_min_cmd_duration 1000
+
 switch (uname)
 case Linux
-    ~/.local/bin/mise activate fish | source
+    $HOME/.local/bin/mise activate fish | source
 case Darwin
     fish_add_path /opt/homebrew/bin $HOME/.local/bin /Applications/WezTerm.app/Contents/MacOS/
 end
 
-set fish_command_timer_min_cmd_duration 1000
+fish_add_path $HOME/.cargo/bin
 
